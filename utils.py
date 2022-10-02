a903b3b2b6e83f0cd1af38b955e8f9d84651120b
simba-pumba <54436635+simba-pumba@users.noreply.github.com>
2022-10-02 16:02:01 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 import random
 import numpy as np
 import torch
+import re
 
 
 
@@ -36,7 +37,7 @@ def set_arguments():
                         help='model weight file name')
 
 
-    parser.add_argument('-predction', 
+    parser.add_argument('-prediction', 
                         action='store_true',
                         help='(test / prediction) ')
     
@@ -58,15 +59,60 @@ def set_arguments():
                     default=50)
     
     parser.add_argument('-lr', 
-                    default=3e-4)
+                    default=0.001)
 
     parser.add_argument('-batch', 
-                    default=32)
+                    default=32,
+                    type=int)
 
     parser.add_argument('-num_woarkers', 
                     default=0, 
                     type=int)    
-    
+ 
+    parser.add_argument('-momentum', 
+                    default=0.9, 
+                    type=float)   
+
+    parser.add_argument('-weight_decay', 
+                    default=1e-4, 
+                    type=float)      
     return parser.parse_args()
     
     
+# 출처 https://jsikim1.tistory.com/213
+punct = "/-'?!.,#$%\'()*+-/:;<=>@[\\]^_`{|}~" + '""“”’' + '∞θ÷α•à−β∅³π‘₹´°£€\×™√²—–&'
+mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": " sqrt ", "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-", "`": "'", '“': '"', '”': '"', '“': '"', "£": "e", '∞': 'infinity', 'θ': 'theta', '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 'β': 'beta', '∅': '', '³': '3', 'π': 'pi', } 
+
+
+def clean(text):
+    for p in mapping:
+        text = text.replace(p, mapping[p])
+    
+    for p in punct:
+        text = text.replace(p, f' {p} ')
+    
+    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '', 'है': ''}
+    for s in specials:
+        text = text.replace(s, specials[s])
+    
+    return text.strip()
+
+
+def clean_str(text):
+    pattern = '([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)' # E-mail제거
+    text = re.sub(pattern=pattern, repl='', string=text)
+    pattern = '(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+' # URL제거
+    text = re.sub(pattern=pattern, repl='', string=text)
+    pattern = '([ㄱ-ㅎㅏ-ㅣ]+)'  # 한글 자음, 모음 제거
+    text = re.sub(pattern=pattern, repl='', string=text)
+    pattern = '<[^>]*>'         # HTML 태그 제거
+    text = re.sub(pattern=pattern, repl='', string=text)
+    pattern = '[^\w\s\n]'         # 특수기호제거
+    text = re.sub(pattern=pattern, repl='', string=text)
+    text = re.sub('[^가-힣]', ' ', text) # 이모티콘, 특수 기호 제거
+    text = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]','', string=text)
+    text = re.sub('\n', '.', string=text)
+    text = re.sub(r"\s+", " ", text)
+    text = text.rstrip()
+    text = text.lstrip()
+    return text 
