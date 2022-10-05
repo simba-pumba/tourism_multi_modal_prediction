a5617104d27102e8bde4b15a29f1a97b2fd4b218
simba-pumba <54436635+simba-pumba@users.noreply.github.com>
2022-10-05 15:06:57 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,27 +1,27 @@
 import os
 import pandas as pd
-from PIL import Image
+import random
+from tqdm import tqdm
 import torch
-from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.data import DataLoader
-from torchvision import transforms
 from utils import set_arguments, control_randomness
-from dataset import ImageDataset, MultiModalDataset
+from dataset import ImageDataset, NLDataset, MultiModalDataset
+from models import KobertUsingCat2, Kobert
+from transformers import AutoConfig
+# FOR VISION
+from PIL import Image
+from torchvision import transforms
 
-from nts_net import model
-from nts_net.utils import init_log, progress_bar
+# OPTIMIZER FROM TRANSFORMERS
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
 
-# https://dacon.io/competitions/official/235978/codeshare/6565?page=1&dtype=recent
+warmup_ratio = 0.1
+max_grad_norm = 1
+log_interval = 50
 
-# https://ndb796.tistory.com/373
-
-# https://paperswithcode.com/task/multi-modal-document-classification
-# https://github.com/nicolalandro/ntsnet-cub200
-
-SAVE_FREQ = 1
-PROPOSAL_NUM = 6
 def main(args):
-    # set gpu
+    # SET GPU
     if args.gpu>=0:
         if torch.cuda.is_available():
             device = torch.device('cuda')  
@@ -29,150 +29,92 @@ def main(args):
             assert False
     else:
         device = torch.device('cpu')
-    
-    # load data
-    
-    transform_train = transforms.Compose([
-        # transforms.Resize((600, 600), Image.BILINEAR),
-        # transforms.CenterCrop((448, 448)),
-        transforms.Resize((args.img_size, args.img_size), Image.BILINEAR),
-        transforms.RandomHorizontalFlip(),  # solo se train
-        transforms.ToTensor(),
-        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-
-    transform_test = transforms.Compose([
-        # transforms.Resize((600, 600), Image.BILINEAR),
-        # transforms.CenterCrop((448, 448)),
-        transforms.Resize((args.img_size, args.img_size), Image.BILINEAR),
-        # transforms.RandomHorizontalFlip(), # solo se train
-        transforms.ToTensor(),
-        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-
-    train_file = pd.read_csv(f"{args.path}/train_samples.csv")
-    val_file = pd.read_csv(f"{args.path}/val_samples.csv")
-    if args.multimodal:
-        train_dataset = MultiModalDataset(train_file, transform_train, prediction=False)
-        val_dataset = MultiModalDataset(val_file, transform_test, prediction=False)
-    else:
-        train_dataset = ImageDataset(train_file, transform_train, prediction=False)
-        val_dataset = ImageDataset(val_file, transform_test, prediction=False)
-    
-    train_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True, num_workers=args.num_woarkers)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch, shuffle=False, num_workers=args.num_woarkers)
-    
-    
-    nets = model.attention_net(topN=PROPOSAL_NUM, num_classes=128, device=device)
-
-    if args.resume:
-            ckpt = torch.load(args.ckpt)
-            nets.load_state_dict(ckpt['net_state_dict'])
-            start_epoch = ckpt['epoch'] + 1
-    else:
-        start_epoch = 0
         
-    nets = nets.to(device)
-    creterion = torch.nn.CrossEntropyLoss()
+    train_dataset = NLDataset(args.train_path, device, prediction=False)
+    val_dataset = NLDataset(args.val_path, device, prediction=False)
     
-    if args.prediction:
-        pass
-    else:
-        trainer(train_loader, val_loader, nets, start_epoch, creterion, args.lr, args.epochs, args.momentum, args.weight_decay, device)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True, num_workers=args.num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch, num_workers=args.num_workers)
+    # model = KobertUsingCat2(AutoConfig.from_pretrained("monologg/kobert")).to(device)
+    model = Kobert(AutoConfig.from_pretrained("monologg/kobert")).to(device)
+
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+
+    optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr)
+    t_total = len(train_dataloader) * args.epochs
     
-def trainer(train_loader, val_loader, nets, start_epoch, creterion, LR, EPOCHS, MMT, WD, device):
 
-    # define optimizers
-    raw_parameters = list(nets.pretrained_model.parameters())
-    part_parameters = list(nets.proposal_net.parameters())
-    concat_parameters = list(nets.concat_net.parameters())
-    partcls_parameters = list(nets.partcls_net.parameters())
+    t_total = len(train_dataloader) * args.epochs
+    warmup_step = int(t_total * warmup_ratio)
 
-    raw_optimizer = torch.optim.SGD(raw_parameters, lr=LR, momentum=MMT, weight_decay=WD)
+    scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
 
-    concat_optimizer = torch.optim.SGD(concat_parameters, lr=LR, momentum=MMT, weight_decay=WD)
 
-    part_optimizer = torch.optim.SGD(part_parameters, lr=LR, momentum=MMT, weight_decay=WD)
-
-    partcls_optimizer = torch.optim.SGD(partcls_parameters, lr=LR, momentum=MMT, weight_decay=WD)
-
-    schedulers = [MultiStepLR(raw_optimizer, milestones=[60, 100], gamma=0.1),
-                  MultiStepLR(concat_optimizer, milestones=[60, 100], gamma=0.1),
-                  MultiStepLR(part_optimizer, milestones=[60, 100], gamma=0.1),
-                  MultiStepLR(partcls_optimizer, milestones=[60, 100], gamma=0.1)]
+    # train_history=[]
+    val_history=[]
+    # loss_history=[]
+    max_val_acc = 0.0
     
-    for epoch in range(start_epoch, EPOCHS):
-        for scheduler in schedulers:
-            scheduler.step()
+    dir_name = os.path.join(args.save_path, f"Kobert_{random.randrange(10000,99000)}")
+    os.mkdir(dir_name)
+    
+    for epoch in range(args.epochs):
+        train_acc = 0.0
+        val_acc = 0.0
+        model.train()
+        for batch_id, inputs in enumerate(tqdm(train_dataloader)):
+            optimizer.zero_grad()
+            loss, logits = model(**inputs)
         
-        nets.train()
-        for i, data in enumerate(train_loader):
-            img, label = data[0].to(device), data[1].to(device)
-            batch_size = img.size(0)
-            raw_optimizer.zero_grad()
-            part_optimizer.zero_grad()
-            concat_optimizer.zero_grad()
-            partcls_optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            optimizer.step()
+            scheduler.step()  # Update learning rate schedule
             
-            _, _, raw_logits, concat_logits, part_logits, _, top_n_prob = nets(img)
-            part_loss = model.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
-                                        label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1)).view(batch_size,
-                                                                                                  PROPOSAL_NUM)
-            raw_loss = creterion(raw_logits, label)
-            concat_loss = creterion(concat_logits, label)
-            rank_loss = model.ranking_loss(top_n_prob, part_loss)
-            partcls_loss = creterion(part_logits.view(batch_size * PROPOSAL_NUM, -1),
-                                     label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1))
+            train_acc += calc_accuracy(logits, inputs['labels'])
+            
+            if batch_id % log_interval == 0:
+                print("epoch {} batch id {} loss {} train acc {}".format(epoch+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+                # train_history.append(train_acc / (batch_id+1))
+                # loss_history.append(loss.data.cpu().numpy())
+                
+        print("epoch {} train acc {}".format(epoch+1, train_acc / (batch_id+1)))
+        #train_history.append(train_acc / (batch_id+1))
+        
+        model.eval()
+        for batch_id, inputs in enumerate(tqdm(val_dataloader)):
+            loss, logits = model(**inputs)
+            val_acc += calc_accuracy(logits, inputs['labels'])
+        val_acc = val_acc / (batch_id+1)
+        print("epoch {} test acc {}".format(epoch+1, val_acc))
+        if val_acc> max_val_acc:
+            max_val_acc = val_acc
+            
+            torch.save(model.state_dict(), 'model.pth')
+            model.load_state_dict(torch.load('model.pth'))
 
-            total_loss = raw_loss + rank_loss + concat_loss + partcls_loss
-            total_loss.backward()
-            raw_optimizer.step()
-            part_optimizer.step()
-            concat_optimizer.step()
-            partcls_optimizer.step()
+            torch.save({'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            },
+            os.path.join(dir_name, f"Kobert_epoch_{epoch+1}_val_acc_{max_val_acc}.pth"))
             
-            progress_bar(i, len(train_loader), 'train')
-            print(f'Batch {i}/{len(train_loader)}   Loss: {total_loss.item()}')
+        val_history.append(val_acc)
     
-        nets.eval()
-        test_loss = 0
-        test_correct = 0
-        total = 0
-        for i, data in enumerate(val_loader):
-            with torch.no_grad():
-                img, label = data[0].to(device), data[1].to(device)
-                batch_size = img.size(0)
-                _, _, concat_logits, _, _, _, _ = nets(img)
-                # calculate loss
-                concat_loss = creterion(concat_logits, label)
-                # calculate accuracy
-                _, concat_predict = torch.max(concat_logits, 1)
-                total += batch_size
-                test_correct += torch.sum(concat_predict.data == label.data)
-                test_loss += concat_loss.item() * batch_size
-                progress_bar(i, len(val_loader), 'eval test set')
-        test_acc = float(test_correct) / total
-        test_loss = test_loss / total
-        print(
-            'epoch:{} - test loss: {:.3f} and test acc: {:.3f} total sample: {}'.format(
-                epoch,
-                test_loss,
-                test_acc,
-                total))
-        ##########################  save model  ###############################
-        net_state_dict = nets.state_dict()
-        if epoch % SAVE_FREQ == 0:
-            torch.save({
-                'epoch': epoch,
-                # 'train_loss': train_loss,
-                # 'train_acc': train_acc,
-                'test_loss': test_loss,
-                'test_acc': test_acc,
-                'net_state_dict': net_state_dict},
-                os.path.join("./save_model/", '%03d.ckpt' % epoch))
-    print('finishing training')
-    
-if __name__=="__main__":
+
+def test(args):
+    pass
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+if __name__ == "__main__":
     args = set_arguments()
     
 
@@ -180,6 +122,10 @@ if __name__=="__main__":
         if args.checkpoint == "":
             assert False
     
-    control_randomness(args.seed)
+    # control_randomness(args.seed)
     
-    main(args)
+    if args.prediction:
+        test(args)
+    else:
+        main(args)
+        
